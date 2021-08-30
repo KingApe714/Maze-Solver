71c4ceda413ff022138b7bb4904c395e2d1a0db8
jonathandiaz <jonathan.diaz714@gmail.com>
2021-08-29 22:21:54 -0400

    testing

--- src/MazeGame.js
+++ src/MazeGame.js
@@ -9,6 +9,13 @@ class Maze {
     startMaze(){
         // starts all the logic you can start manipulating the dom in side of here
         console.log("Starting maze here " ); 
+        const testOuterDiv = document.querySelector(".test-outer-div")
+    const testDiv = document.createElement('div');
+    
+    testDiv.innerHTML = "Hello RYAN THIS IS WORKING";
+    testOuterDiv.appendChild(testDiv)
+    testOuterDiv.innerHTML = "SOMETHING!!"
+    maze.startMaze(); 
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import Maze from './MazeGame.js'; //Handles all logic for your maze
 
 document.addEventListener("DOMContentLoaded", ()=> {
     const maze = new Maze(); //maze is a class that exists inside of MazeGameLogic.js 
-
+    const testOuterDiv = document.querySelector(".test-outer-div")
+    const testDiv = document.createElement('div');
+    testDiv.style.backgroundColor = "green";
+    testDiv.style.width = "100px";
+    testDiv.style.height = "100px";
+    testDiv.innerHTML = "Hello RYAN THIS IS WORKING";
+    console.log(testDiv)
+    // testOuterDiv.innerHTML = "SOMETHING!!"
+    // testOuterDiv.appendChild(testDiv)
+    testOuterDiv.appendChild(testDiv)
+    console.log(testOuterDiv.children)
     maze.startMaze(); 
     console.log("DOM is loaded"); 
 })
