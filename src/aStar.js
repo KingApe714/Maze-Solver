5f94e24606f2f0860a3885e0d8b36730f48e0669
jonathandiaz <jonathan.diaz714@gmail.com>
2021-08-30 16:13:08 -0400

    set up nodes and neighbors

--- src/aStar.js
+++ src/aStar.js
@@ -10,14 +10,37 @@ function node(value, coordinates) {
 }
 
 export const aStar = (grid) => {
+    //set up neighbor check
+    const nCheck = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+        [0, 1],
+        [0, -1]
+    ]
     for (let i = 0; i < grid.length; i++) {
         for (let j = 0; j < grid[0].length; j++) {
             console.log(`[${i},${j}] ${grid[i][j].innerHTML}`)
             let arr = []
             arr.push(grid[i][j])
             arr.push(new node(grid[i][j].innerHTML, `${i},${j}`))
-            arr[1].neighbors.push()
             grid[i][j] = arr
+        }
+    }
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            nCheck.forEach(n => {
+                let x = n[0] + i;
+                let y = n[1] + j;
+                if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length) {
+                    grid[i][j][1].neighbors.push(grid[x][y])
+                }
+            })
+
+            console.log(grid[i][j])
         }
     }
 
