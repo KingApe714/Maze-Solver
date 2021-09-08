7c7e5f2c2a269659486e7c12f1feb64df02d102c
jonathandiaz <jonathan.diaz714@gmail.com>
2021-09-08 16:12:31 -0400

    fix highlight path bug

--- src/MazeGame.js
+++ src/MazeGame.js
@@ -19,7 +19,7 @@ class Maze {
 
         //tiles are the divs
         //nodes are the nodes
-        //and the cells hold both the tiles and the nodes in an array lenth 2
+        //and the cells hold both the tiles and the nodes in an array length 2
 
         let startSelected = false;
         let endSelected = false;
@@ -52,13 +52,13 @@ class Maze {
         testOuterDiv.addEventListener(mouseDown, () => {
             mouseDown = true;
         })
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 15; i++) {
             let row = []
-            for (let j = 0; j < 10; j++) {
+            for (let j = 0; j < 20; j++) {
                 let tile = document.createElement('div')
                 tile.className = "tile";
-                tile.style.left = j * 50 + "px";
-                tile.style.top = i * 50 + "px";
+                tile.style.left = j * 45 + "px";
+                tile.style.top = i * 45 + "px";
                 tile.innerHTML = `[${i}, ${j}]`
                 
                 tile.addEventListener('click', () => {
@@ -66,8 +66,10 @@ class Maze {
                         tile.style.backgroundColor = "blue"
                         endSelected = false;
                         if (start) {
-                            grid[parseInt(start[0])][parseInt(start[2])][0].style.backgroundColor = "red"
-                            grid[parseInt(start[0])][parseInt(start[2])][1].startPoint = false;
+                            let [sX, sY] = start.split(',')
+                            console.log(`xS = ${sX} xY = ${sY}`)
+                            grid[sX][sY][0].style.backgroundColor = "red"
+                            grid[sX][sY][1].startPoint = false;
                         }
                         grid[i][j][1].startPoint = true
                         startTile = tile;
@@ -76,8 +78,9 @@ class Maze {
                         tile.style.backgroundColor = "yellow"
                         startSelected = false;
                         if (end) {
-                            grid[parseInt(end[0])][parseInt(end[2])][0].style.backgroundColor = "red"
-                            grid[parseInt(end[0])][parseInt(end[2])][1].endPoint = false;
+                            let [eX, eY] = end.split(',')
+                            grid[eX][eY][0].style.backgroundColor = "red"
+                            grid[eX][eY][1].endPoint = false;
                         }
                         grid[i][j][1].endPoint = true;
                         endTile = tile;
@@ -118,12 +121,14 @@ class Maze {
         setUpGrid(grid)
 
         goButton.addEventListener('click', () => {
-            startTile = grid[parseInt(start[0])][parseInt(start[2])]
-            endTile = grid[parseInt(end[0])][parseInt(end[2])]
+            let [sX, sY] = start.split(',')
+            let [eX, eY] = end.split(',')
+            startTile = grid[sX][sY]
+            endTile = grid[eX][eY]
             aStar(startTile, endTile, wallCells)
         })
 
-        console.log(grid)
+        // console.log(grid)
     }
     
 }
--- src/aStar.js
+++ src/aStar.js
@@ -2,7 +2,7 @@
 function node(value, coordinates) {
     this.parent = null;
     this.neighbors = [];
-    this.g = 0;
+    this.g = null;
     this.h = 0;
     this.f = 0;
     this.value = value;
@@ -62,10 +62,14 @@ export const aStar = (startCell, endCell, wallCells) => {
     let path = [];
     let visitedCells = [];
     let checkQueue = [checkCell];
+    
+    let count = 0
 
     visitedCells.push(startCell[1].coordinates)
     path.push(startCell)
-    while (checkCell[1] !== endNode) {
+    while (checkCell[1] !== endNode && count !== 30) {
+
+        // console.log(`count = ${count}`)
 
         checkQueue.sort((first, second) => {
             if (first[1].f !== second[1].f) {
@@ -81,6 +85,7 @@ export const aStar = (startCell, endCell, wallCells) => {
                 gSetter(n[1])
                 hSetter(endNode, n[1])
             }
+            // console.log(`${n[1].coordinates}.h = ${n[1].h}`)
             n[1].f = n[1].g + n[1].h;
             n[0].innerHTML += `<br> g:${n[1].g} <br> h:${n[1].h} <br> f:${n[1].f}`
             if (!visitedCells.includes(n[1].coordinates) && !checkQueue.includes(n) && !n[1].isWall) {
@@ -90,6 +95,9 @@ export const aStar = (startCell, endCell, wallCells) => {
         })
         if (!visitedCells.includes(checkCell[1].coordinates)) visitedCells.push(checkCell[1].coordinates)
         path.push(checkCell);
+
+        count++
+        // console.log(count)
     }
     path.forEach(cell => {
         // console.log(cell[1].coordinates)
@@ -101,29 +109,43 @@ export const aStar = (startCell, endCell, wallCells) => {
 
 function highlightPath(startCell, endCell) {
     let currentCell = endCell;
-    while (currentCell !== startCell) {
+    let count = 0
+    while (currentCell !== startCell && count !== 30) {
         currentCell[0].style.backgroundColor = "white"
+
+        if (currentCell[0].innerHTML[currentCell[0].innerHTML.length - 1] == ']') currentCell[0].innerHTML += `g = ${currentCell[1].g}`
+
         let cellCheck = null;
         currentCell[1].neighbors.forEach(n => {
-            console.log(cellCheck)
-            if (!n[1].isWall && (cellCheck === null || cellCheck[1].g > n[1].g)) {
+            // console.log(cellCheck)
+            if (n[1].g !== null && !n[1].isWall && (cellCheck === null || cellCheck[1].g > n[1].g)) {
                 cellCheck = n
             }
         })
         currentCell = cellCheck;
+
+        count++
+
     }
     startCell[0].style.backgroundColor = "white"
 }
 
 function gSetter(node) {
     // console.log(`${node.coordinates} = ${node}`)
-    let nodeX = node.coordinates[0]
-    let nodeY = node.coordinates[2]
+    // let nodeX = node.coordinates[0]
+    // let nodeY = node.coordinates[2]
+
+    let [nodeX, nodeY] = node.coordinates.split(',')
 
     node.neighbors.forEach(n => {
         //node is in position 1 of each n
-        let nX = n[1].coordinates[0]
-        let nY = n[1].coordinates[2]
+        // let nX = n[1].coordinates[0]
+        // let nY = n[1].coordinates[2]
+
+        let [nX, nY] = n[1].coordinates.split(',')
+
+        if (n[1].g === null) n[1].g = 0
+
         if (nodeX === nX || nodeY === nY) {
             if (!n[1].startPoint && (node.g + 10 < n[1].g || n[1].g === 0)) {
                 n[1].g = node.g + 10
@@ -137,15 +159,30 @@ function gSetter(node) {
 }
 
 function hSetter(endNode, node) {
-    let endX = endNode.coordinates[0];
-    let endY = endNode.coordinates[2];
-    let nodeX = node.coordinates[0];
-    let nodeY = node.coordinates[2];
+    // let endX = endNode.coordinates[0];
+    // let endY = endNode.coordinates[2];
+
+    let [endX, endY] = endNode.coordinates.split(',')
+    // let nodeX = node.coordinates[0];
+    // let nodeY = node.coordinates[2];
+
+    // console.log(endX)
+    // console.log(endY)
+    
+    let [nodeX, nodeY] = node.coordinates.split(',')
+
+    // console.log(node.value)
+    // console.log(parseInt(nodeX))
+
     let deltaX = Math.abs(endX - nodeX);
     let deltaY = Math.abs(endY - nodeY);
+
+    // console.log(deltaX)
 
     let adj = Math.abs(deltaX - deltaY);
     let diag = Math.min(deltaX, deltaY);
 
     node.h = (adj * 10) + (diag * 14);
+
+    // console.log(node.h)
 }
